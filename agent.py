36a8418f65d4bda59d2153a5421abe7385516f02
Zhou Qiang <zhouqiang.cl@gmail.com>
2017-11-08 10:41:04 +0800

    fix agent bugs

--- agent.py
+++ agent.py
@@ -7,23 +7,29 @@ import tornado.concurrent
 import tornado.gen
 
 from libs.misc import system
-from iexceptions import PluginNotExistsException
+from iexceptions import PluginNotExistsException,PluginSingletonException,CommandInvalidateException
 
 class Runner(object):
     executor = ThreadPoolExecutor(max_workers=24)
     def __init__(self):
         self._plugin_dir = "./plugin"
-        self._running_queue = []
+        self._running_queue = {}
         self._isolate = []
     @tornado.concurrent.run_on_executor
     def _async_execute(self,cmd):
         plugin = cmd.split()[0]
         if plugin in self._isolate:
             self._running_queue[plugin] += 1
-        # result = system(cmd)
-        print cmd
-        self._running_queue[plugin] -= 1
-        return result
+            print "run script plugin here..."
+            self._running_queue[plugin] -= 1
+        else:
+            print "run script plugin here..."
+        return {"result":True}
+
+    def singleton_running(self,plugin):
+        if plugin in self._running_queue and self._running_queue[plugin] > 0:
+            return False
+        return True
 
     @tornado.gen.coroutine
     def run_cmd(self, cmd):
@@ -32,9 +38,9 @@ class Runner(object):
         plugin = cmd.split()[0]
         if not os.path.exists(self._plugin_dir + "/" + plugin):
             raise PluginNotExistsException(plugin_name = plugin)
-        if self._running_queue[plugin] > 1:
+        if not self.singleton_running(plugin):
             raise PluginSingletonException(plugin_name = plugin)
-        result = self._async_execute(cmd)
+        result = yield self._async_execute(cmd)
         raise tornado.gen.Return(result)
 
     def _check_valid(self, cmd):
@@ -48,6 +54,7 @@ class AgentHandler(tornado.web.RequestHandler):
     def get(self):
         cmd = self.get_argument("cmd")
         result = yield self._runner.run_cmd(cmd)
+        print result
         self.finish(result)
 
 def make_app():

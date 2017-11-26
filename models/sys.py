fffaf1d5e38741caa23b17fe7fc26a9a6a948e22
Zhou <zhouqiang@wandoujia.com>
2017-11-26 10:51:47 +0800

    add cgroup

--- models/sys.py
+++ models/sys.py
@@ -1,10 +1,10 @@
 from libs.misc import system
 class Sys(object):
     def get_block_number(self, block):
-        "ls -lhrt"
+        # "ls -lhrt"
         cmd = "ls -lhrt {block}".format(block)
         # print "start running command '{cmd}'".format(cmd=cmd)
-        "brw-rw----. 1 root disk 8, 16 Nov  6 11:57 /dev/sdb"
+        # "brw-rw----. 1 root disk 8, 16 Nov  6 11:57 /dev/sdb"
         rc,so,se = system(cmd)
         print rc, so, se
         return "".join(a.split()[4:6]).replace(",",":")
@@ -16,7 +16,7 @@ class Sys(object):
             if not line:
                 continue
             if line.split()[2] == mount:
-                return line.split()
+                return line.split()[0]
 
 if __name__ == "__main__":
     sys = Sys()

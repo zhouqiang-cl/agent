bfc3f1c73c4257442e736191ac18898cf13c5dad
Zhou <zhouqiang@wandoujia.com>
2017-11-29 23:28:52 +0800

    use a different method to inject disk error

--- plugin/cgroup_disk.py
+++ plugin/cgroup_disk.py
@@ -12,21 +12,39 @@ class DiskExecutor(models.executor.Executor):
 
     def fail(self, operation, **kwargs):
         """umount"""
+        # dirname = kwargs["dirname"] if "dirname" in kwargs and kwargs["dirname"] else None
+        # if not dirname:
+        #     return
+        # container_id = kwargs["container_id"] if "container_id" in kwargs and kwargs["container_id"] else None
+        # mount_dir = docker.get_mount_dir(container_id, dirname)
+        # # print mount_dir
+        # link = sys.get_link(mount_dir)
+        # # print "mount:", mount_dir, "link:", link
+        # if operation == "start":
+        #     agent.set_link(container_id, mount_dir, link)
+        #     agent.unlink(mount_dir)
+        #     # unlink  tikv-dir-4 && ln -s /mnt/noexist tikv-dir-4
+        # else:
+        #     origin = agent.get_link(container_id, mount_dir)
+        #     sys.relink(mount_dir, origin)
         dirname = kwargs["dirname"] if "dirname" in kwargs and kwargs["dirname"] else None
         if not dirname:
             return
         container_id = kwargs["container_id"] if "container_id" in kwargs and kwargs["container_id"] else None
+        rate = 1
+        # if rate == "None":
+        #     rate = 1048576
+        # if operation == "stop":
+        #     rate = 0
+        cgroup_path = docker.get_cgroup_path(container_id) + "/" + "blkio.throttle.read_bps_device"
         mount_dir = docker.get_mount_dir(container_id, dirname)
-        # print mount_dir
-        link = sys.get_link(mount_dir)
-        # print "mount:", mount_dir, "link:", link
-        if operation == "start":
-            agent.set_link(container_id, mount_dir, link)
-            agent.unlink(mount_dir)
-            # unlink  tikv-dir-4 && ln -s /mnt/noexist tikv-dir-4
-        else:
-            origin = agent.get_link(container_id, mount_dir)
-            sys.relink(mount_dir, origin)
+        mount_dir = sys.get_link(mount_dir)
+        block = sys.get_block_by_mount_in_docker(mount_dir)
+        block_num = sys.get_block_number(block)
+        data = block_num + " " + str(rate)
+        sys.write_to_cgroup(data, cgroup_path)
+        cgroup_path = docker.get_cgroup_path(container_id) + "/" + "blkio.throttle.write_bps_device"
+        sys.write_to_cgroup(data, cgroup_path)
             
 
     def limit(self, operation, **kwargs):

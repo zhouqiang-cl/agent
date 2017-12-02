c64b38aa58c1090c49cb31572b7714a721037778
Zhou <zhouqiang@wandoujia.com>
2017-12-02 21:58:11 +0800

    clean agent code

--- agent.py
+++ agent.py
@@ -92,9 +92,35 @@ class Runner(object):
 
 runner = Runner()
 
-class DiskHandler(tornado.web.RequestHandler):
+class PluginHanlder(tornado.web.RequestHandler):
     def prepare(self):
         self._runner = runner
+
+    @tornado.gen.coroutine
+    def run_plugin(self, container_id, plugin, action, operation, plugin_cmd, add_on):
+        if operation == "start":
+            if self._runner.check_lock(container_id):                
+                msg = plugin + ":" + action + ":" + add_on
+                try:
+                    self._runner.require_lock(container_id, msg)
+                    result = yield self._runner.run_cmd(cmd)
+                    self.finish(result)
+                except ExecuteException as e:
+                    self._runner.delete_lock(container_id, msg )
+                    self.finish({"status":"failed","msg":e._msg})
+            else:
+                lock_msg = self._runner.get_container_lock_msg(container_id).split(":")
+                msg = "there is a {job_type} job running for {container_id},operation is {operation}, additional msg is {add_msg}".format(
+                    job_type = lock_msg[0], container_id=container_id, operation=lock_msg[1], add_msg = lock_msg[2]) 
+                self.finish({"status":"failed","msg":msg})
+        elif operation == "stop":
+            msg = plugin +":" + action + ":" + add_on
+            result = yield self._runner.run_cmd(cmd)
+            self._runner.delete_lock(container_id, msg )
+            self.finish(result)
+
+class DiskHandler(PluginHanlder):
+    @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
         """./plugin/cgroup_disk.py -a limit -d /test-pd -c b63085b8ad9b540ee3603572ca95c2552061c1415564834c8ca3c9e578e7400c start"""
@@ -109,29 +135,10 @@ class DiskHandler(tornado.web.RequestHandler):
         else:
             cmd = "disk.py -a {action} -d {dirname} -c {container_id} {operation}".format(action=action, dirname=dirname, 
                 container_id=container_id, operation=operation)
-        if operation == "start":
-            if self._runner.check_lock(container_id):                
-                msg = "disk:" + action + ":" + dirname
-                try:
-                    result = yield self._runner.run_cmd(cmd)
-                    self._runner.require_lock(container_id, msg )
-                    self.finish(result)
-                except ExecuteException as e:
-                    self.finish({"status":"failed","msg":e._msg})
-            else:
-                lock_msg = self._runner.get_container_lock_msg(container_id).split(":")
-                msg = "there is a {job_type} job running for {container_id},operation is {operation}, additional msg is {add_msg}".format(
-                    job_type = lock_msg[0], container_id=container_id, operation=lock_msg[1], add_msg = lock_msg[2]) 
-                self.finish({"status":"failed","msg":msg})
-        elif operation == "stop":
-            msg = "disk:" + action + ":" + dirname
-            result = yield self._runner.run_cmd(cmd)
-            self._runner.delete_lock(container_id, msg )
-            self.finish(result)
+        self.run_plugin(container_id, "disk", action, operation, cmd,dirname)
 
-class NetworkHandler(tornado.web.RequestHandler):
-    def prepare(self):
-        self._runner = runner
+class NetworkHandler(PluginHanlder):
+    @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
         action = self.get_argument("action")
@@ -145,29 +152,10 @@ class NetworkHandler(tornado.web.RequestHandler):
         else:
             cmd = "network.py -a {action} --container_ip {container_ip} -r {rate} {operation}".format(action=action, 
                 container_ip=container_ip, operation=operation, rate=rate)
-        if operation == "start":
-            if self._runner.check_lock(container_id):                
-                msg = "network:" + action + ":" + container_ip
-                try:
-                    result = yield self._runner.run_cmd(cmd)
-                    self._runner.require_lock(container_id, msg )
-                    self.finish(result)
-                except ExecuteException as e:
-                    self.finish({"status":"failed","msg":e._msg})
-            else:
-                lock_msg = self._runner.get_container_lock_msg(container_id).split(":")
-                msg = "there is a {job_type} job running for {container_id},operation is {operation}, additional msg is {add_msg}".format(
-                    job_type = lock_msg[0], container_id=container_id, operation=lock_msg[1], add_msg = lock_msg[2]) 
-                self.finish({"status":"failed","msg":msg})
-        elif operation == "stop":
-            msg = "network:" + action + ":" + container_ip
-            result = yield self._runner.run_cmd(cmd)
-            self._runner.delete_lock(container_id, msg )
-            self.finish(result)
+        self.run_plugin(container_id, "network", action, operation, cmd, container_ip)
 
-class CpuHandler(tornado.web.RequestHandler):
-    def prepare(self):
-        self._runner = runner
+class CpuHandler(PluginHanlder):
+    @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
         action = self.get_argument("action")
@@ -181,28 +169,9 @@ class CpuHandler(tornado.web.RequestHandler):
         else:
             cmd = "cpu.py -a {action} --containerid {container_id} {operation}".format(action=action, 
                 container_id=container_id, operation=operation)
-        if operation == "start":
-            if self._runner.check_lock(container_id):                
-                msg = "network:" + action + ":noop"
-                try:
-                    result = yield self._runner.run_cmd(cmd)
-                    self._runner.require_lock(container_id, msg )
-                    self.finish(result)
-                except ExecuteException as e:
-                    self.finish({"status":"failed","msg":e._msg})
-            else:
-                lock_msg = self._runner.get_container_lock_msg(container_id).split(":")
-                msg = "there is a {job_type} job running for {container_id},operation is {operation}, additional msg is {add_msg}".format(
-                    job_type = lock_msg[0], container_id=container_id, operation=lock_msg[1], add_msg = lock_msg[2]) 
-                self.finish({"status":"failed","msg":msg})
-        elif operation == "stop":
-            msg = "network:" + action + ":noop"
-            result = yield self._runner.run_cmd(cmd)
-            self._runner.delete_lock(container_id, msg )
-            self.finish(result)
-class MemoryHandler(tornado.web.RequestHandler):
-    def prepare(self):
-        self._runner = runner
+        self.run_plugin(container_id, "cpu", action, operation, cmd, "noop")
+class MemoryHandler(PluginHanlder):
+    @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
         action = self.get_argument("action")
@@ -216,25 +185,7 @@ class MemoryHandler(tornado.web.RequestHandler):
         else:
             cmd = "mem.py -a {action} --containerid {container_id} {operation}".format(action=action, 
                 container_id=container_id, operation=operation)
-        if operation == "start":
-            if self._runner.check_lock(container_id):                
-                msg = "memory:" + action + ":noop"
-                try:
-                    result = yield self._runner.run_cmd(cmd)
-                    self._runner.require_lock(container_id, msg )
-                    self.finish(result)
-                except ExecuteException as e:
-                    self.finish({"status":"failed","msg":e._msg})
-            else:
-                lock_msg = self._runner.get_container_lock_msg(container_id).split(":")
-                msg = "there is a {job_type} job running for {container_id},operation is {operation}, additional msg is {add_msg}".format(
-                    job_type = lock_msg[0], container_id=container_id, operation=lock_msg[1], add_msg = lock_msg[2]) 
-                self.finish({"status":"failed","msg":msg})
-        elif operation == "stop":
-            msg = "memory:" + action + ":noop"
-            result = yield self._runner.run_cmd(cmd)
-            self._runner.delete_lock(container_id, msg )
-            self.finish(result)
+        self.run_plugin(container_id, "cpu", action, operation, cmd, "noop")
 
 class SupportApis(tornado.web.RequestHandler):
     def get(self):

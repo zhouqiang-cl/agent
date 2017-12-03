c3e0b15bec4c4b484885f592035d59a7a8d7dbee
Zhou <zhouqiang@wandoujia.com>
2017-12-03 22:15:30 +0800

    fix container_id

--- models/agent.py
+++ models/agent.py
@@ -6,13 +6,13 @@ class Agent(object):
     def __init__(self):
         self._prefix = "/host"
 
-    def set_full(self, mount_dir):
+    def set_full(self, dirname):
         cmd =  "dd if=/dev/zero of={dirname}/tst.img bs=4M count=27K"(dirname = self._prefix + dirname)
         rc,so,se = system(cmd)
         if rc:
             raise ExecuteException(msg = so)
         return True
-    def set_empty(self, mount_dir):
+    def set_empty(self, dirname):
         if not os.path.exists(self._prefix + dirname):
             return True
         cmd = "rm -f {dirname}/tst.img".format(dirname = self._prefix + dirname)
